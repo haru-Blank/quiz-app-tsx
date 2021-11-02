059268f0e2710cf9b2e990be7d4176444b573be3
Harendra Bhujel <avash.nekros274@gmail.com>
2021-11-03 04:37:21 +0545

    set states and function skeleton in App.jsx file required for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+//!  types
+import { QuestionsState } from './API';
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 
 const App = () => {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
-export default App
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: any) => {};
+
+  const nextQuestion = () => {};
+
+  return <div>Hello World!</div>;
+};
+
+export default App;
